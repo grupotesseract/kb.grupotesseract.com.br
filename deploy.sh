7eefda429c8b28d15613e525eb5b49d8a06b4e4a
Leandro Acquati <leandroacquati@gmail.com>
2019-03-30 02:31:15 -0300

    Configuration Travis CI

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,11 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:grupotesseract/vuepress-tesseract.git master:gh-pages
+# git push -f git@github.com:grupotesseract/vuepress-tesseract.git master:gh-pages
 
-cd -
+# Forçando o push do master para a branch gh-pages (Toda história anterior da branch
+# gh-pages será perdido, pois vamos substituí-lo.)  Redirecionamos qualquer saída para
+# /dev/null para ocultar quaisquer dados de credenciais sensíveis que de outra forma possam ser expostos.
+# tokens GH_TOKEN e GH_REF serão fornecidos como variáveis de ambiente Travis CI
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 
