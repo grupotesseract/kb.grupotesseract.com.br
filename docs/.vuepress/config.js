ce818d83a23a7b04ee5976f4df7fc83794942149
Leandro Acquati <leandroacquati@gmail.com>
2019-03-29 23:52:23 -0300

    Fix config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: '/grupotesseract/vuepress-tesseract/'
+    base: '/grupotesseract/vuepress-tesseract/',
     title: 'Open KB Grupo Tesseract',
     description: 'Documentação e Tutoriais',
     themeConfig: {
