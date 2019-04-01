2e967c2a6b9dbfad3c87e99df4aa4686e832055b
Leandro Acquati <leandroacquati@gmail.com>
2019-04-01 02:41:07 -0300

    Update Home

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/vuepress-tesseract/',
-    title: 'Open KB<br>Grupo Tesseract',
+    title: 'Open KB Grupo Tesseract',
     description: 'Documentação e Tutoriais',
     themeConfig: {
       sidebar: 'auto'
